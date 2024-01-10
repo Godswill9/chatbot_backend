25293fa7f8aef6ffbd19bc4dd074cebdcb159d2b
Groupprojectfirst <groupproject288@gmail.com>
2024-01-10 17:34:21 +0100

    cors fix

--- server.js
+++ server.js
@@ -14,22 +14,22 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5500",
-      "http://localhost:5501",
-      "http://127.0.0.1:5500",
-      "http://127.0.0.1:5501",
-      "https://simpleasoro.netlify.app",
-      "https://premiumasoro.netlify.app",
-      "https://asoroautomotive.com/",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "http://localhost:5500",
+//       "http://localhost:5501",
+//       "http://127.0.0.1:5500",
+//       "http://127.0.0.1:5501",
+//       "https://simpleasoro.netlify.app",
+//       "https://premiumasoro.netlify.app",
+//       "https://asoroautomotive.com/",
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 
 app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
