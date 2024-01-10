cab70015dce84ddbb292c0b18d11789cee6700ce
Groupprojectfirst <groupproject288@gmail.com>
2024-01-10 17:31:40 +0100

    new changes

--- server.js
+++ server.js
@@ -31,6 +31,28 @@ app.use(
   })
 );
 
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins =[
+    "http://localhost:5173",
+    "http://localhost:5500",
+    "http://localhost:5501",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501",
+    "https://simpleasoro.netlify.app",
+    "https://premiumasoro.netlify.app",
+    "https://asoroautomotive.com/",
+  ]
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello server 2");
 });
